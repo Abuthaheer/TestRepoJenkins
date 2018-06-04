d241638b05180186b44bf2daffc6241aef89b1e6
Abuthaheer <abu.sameemullha@wolterskluwer.com>
2018-06-04 20:08:16 +0530

    tests

---
+++ src/test/java/tests/testClass.java
@@ -0,0 +1,17 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class testClass {
+
+	
+	@Test
+	public void testCase1() {
+		System.out.println("Test Case1");
+	}
+	
+	@Test
+	public void testCase2() {
+		System.out.println("Test Case2");
+	}
+}
