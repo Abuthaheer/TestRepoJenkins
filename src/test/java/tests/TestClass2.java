aa163cf23117db64199f6e33dc46b1136de76b92
John Doe <john@sample.com>
2018-08-06 09:51:44 +0530

    Added a new Test Class File

---
+++ src/test/java/tests/TestClass2.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class TestClass2 {
+
+	@Test
+	public void TestCase3() {
+		System.out.println("Test Case 3");
+	}
+
+	@Test
+	public void TestCase4() {
+		System.out.println("Test Case 4");
+	}
+}
