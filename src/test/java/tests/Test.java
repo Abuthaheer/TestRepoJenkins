f253789174a587086a982a7e60189e93c683db5c
NAGARAJAN <NAGARAJAN@pc>
2018-10-03 11:39:53 +0530

    hgdgf

---
+++ src/test/java/tests/Test.java
@@ -0,0 +1,16 @@
+package tests;
+
+
+
+public class Test {
+	
+
+	public void testCase1() {
+		System.out.println("Test Case1");
+	}
+	
+	
+	public void testCase2() {
+		System.out.println("Test Case2");
+}
+}
